2f24a55b9ece91628d2c62d3ea10bb4606392876
Albert Casanovas <caay2000@gmail.com>
2022-05-29 21:07:03 +0200

    REFACTOR vehicle update moved to domain

--- lib/src/main/kotlin/com/github/caay2000/ttk/lib/context/ArrowContextExtension.kt
+++ lib/src/main/kotlin/com/github/caay2000/ttk/lib/context/ArrowContextExtension.kt
@@ -1,19 +1,11 @@
 package com.github.caay2000.ttk.lib.context
 
 import arrow.core.Either
-import arrow.core.Option
-import arrow.core.right
 
 fun <LEFT, RIGHT_A, RIGHT_B> Either<LEFT, RIGHT_A>.context(first: RIGHT_B): Either<LEFT, Pair<RIGHT_B, RIGHT_A>> = this.map { first to it }
 
 inline fun <A, B, C> Either<A, B>.flatNap(f: (B) -> Either<A, C>): Either<A, Pair<B, C>> =
     when (this) {
         is Either.Right -> f(this.value).context(this.value)
-        is Either.Left -> this
-    }
-
-inline fun <A, B, C> Either<A, B>.flatOption(f: (B) -> Option<C>): Either<A, Pair<B, C?>> =
-    when (this) {
-        is Either.Right -> f(this.value).let { res -> this.value to res.orNull() }.right()
         is Either.Left -> this
     }
